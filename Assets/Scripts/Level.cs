871b91add10a6f85639bb09c045ef51e6592da4a
Dmitry-Sm <smirnov.dmitry.a@mail.ru>
2020-04-03 22:18:17 +0300

    Add menu

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public void Start() {
+
+    }
+
+    public static void Finish() {
+        print("Finish");
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject MainMenuPanel;
+    [SerializeField] GameObject LevelListPanel;
+
+    // private void Start() {
+    //     MainMenuPanel = this.transform.GetChild()
+    // }
+
+    public void MainMenuToLevelListButton()
+    {
+        MainMenuPanel.SetActive(false);
+        LevelListPanel.SetActive(true);
+    }
+
+    public void LevelListToMainMenuButton()
+    {
+        MainMenuPanel.SetActive(true);
+        LevelListPanel.SetActive(false);
+    }
+
+    public void LevelButton(int levelNum)
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
